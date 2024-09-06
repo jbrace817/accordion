b4d19f16bc93c2dde6941cd0b4d1325f9d61dabf
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-09-05 22:21:19 -0400

    Add styling to single accordion item
    This will add styling to the component depending if it is open or closed. A chevron react-icon was also added.

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react';
 import styles from './Accordion.module.css';
+import { BiSolidChevronDown } from 'react-icons/bi';
 
 function Accordion() {
   const [isOpen, setIsOpen] = useState(true);
   return (
-    <div className={styles.wrapper}>
-      <h2>1. Render Phase (Virtual DOM Comparison)</h2>
+    <div
+      className={`${styles.wrapper} ${isOpen ? styles.open : styles.closed}`}
+    >
+      <div
+        className={`${styles.title} ${isOpen ? styles.open : styles.closed}`}
+      >
+        <p
+          className={`${styles.number} ${isOpen ? styles.open : styles.closed}`}
+        >
+          01
+        </p>
+        <p>Render Phase (Virtual DOM Comparison)</p>
+        <BiSolidChevronDown size={24} className={styles.icon} />
+      </div>
       {isOpen ? (
         <div>
-          <p>
+          <p className={styles.answer}>
             'When a component updates, React re-renders the component and
             creates a new virtual DOM tree.'
           </p>
