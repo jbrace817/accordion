68835df66996aa7abd3ce72c0528db4f19b6ac0e
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-09-08 23:41:41 -0400

    Add AccordionItem Component.
    A working AccordionItem child component.

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -2,34 +2,61 @@ import { useState } from 'react';
 import styles from './Accordion.module.css';
 import { BiSolidChevronDown } from 'react-icons/bi';
 
-function Accordion() {
-  const [isOpen, setIsOpen] = useState(true);
+function Accordion({ data }) {
+  const [curOpen, setIsOpen] = useState(null);
   return (
     <div
-      className={`${styles.wrapper} ${isOpen ? styles.open : styles.closed}`}
+      className={`${styles.wrapper} ${curOpen ? styles.open : styles.closed}`}
     >
+      {data.map((item, index) => (
+        <AccordionItem
+          title={item.phase}
+          answer={item.text}
+          num={index}
+          curOpen={curOpen}
+          onOpen={setIsOpen}
+          key={index}
+        />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ title, answer, num, curOpen, onOpen }) {
+  const isOpen = num === curOpen;
+
+  function handleOpen() {
+    onOpen(isOpen ? null : num);
+  }
+  return (
+    <>
       <div
         className={`${styles.title} ${isOpen ? styles.open : styles.closed}`}
+        onClick={handleOpen}
       >
-        <p
-          className={`${styles.number} ${isOpen ? styles.open : styles.closed}`}
-        >
-          01
-        </p>
-        <p>Render Phase (Virtual DOM Comparison)</p>
-        <BiSolidChevronDown size={24} className={styles.icon} />
+        <div className={styles.text}>
+          <p
+            className={`${styles.number} ${
+              isOpen ? styles.open : styles.closed
+            }`}
+          >
+            {`0${num + 1}`}
+          </p>
+          <p>{title}</p>
+        </div>
+        <BiSolidChevronDown
+          size={24}
+          className={`${styles.icon} ${isOpen ? styles.open : ''}`}
+        />
       </div>
       {isOpen ? (
         <div>
-          <p className={styles.answer}>
-            'When a component updates, React re-renders the component and
-            creates a new virtual DOM tree.'
-          </p>
+          <p className={styles.answer}>{answer}</p>
         </div>
       ) : (
         ''
       )}
-    </div>
+    </>
   );
 }
 
